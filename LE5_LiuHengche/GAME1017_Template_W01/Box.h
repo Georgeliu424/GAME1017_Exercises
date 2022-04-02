d6b36d5305fbfc947a4a8c1d912ecfe70476f9ee
Georgeliu <liuhengche424@gmail.com>
2022-04-02 12:54:12 -0400

    Added Box.h/.cpp

---
+++ LE5_LiuHengche/GAME1017_Template_W01/Box.h
@@ -0,0 +1,39 @@
+#pragma once
+#ifndef _Box_H_
+#define _Box_H_
+#include<SDL.h>
+
+class Sprite // could be renamed Obstacle for A2
+{
+private:
+	//SDL_Rect m_src;for A2 or use GameObject.h
+	SDL_Rect m_dst;
+	SDL_Color m_color;
+public:
+	Sprite(); // If you have a dynamic array of sprite
+	Sprite(const SDL_Rect r, const SDL_Color c);
+	//void Update();// may need for A2, e.g.spinning blade
+	void Render();
+	friend class Box; //Alternately you can make a setter for m_dst
+};
+
+
+
+class Box // proxy for the obstacle/sprite
+{
+
+private:
+	SDL_Point m_pos; //position on screen .Y is optional
+	Sprite* m_pSprite; // pointer to a dynamic sprite object(s)
+	int m_numSprites; // for bonus of A2
+public:
+		Box(const SDL_Point p, bool makeSprite = false, const SDL_Rect r = {0,0,0,0},
+			const SDL_Color c ={255,255,255,255});
+		~Box();
+		Box* Clone(); //we're making box also a prototype
+		void Update();
+		void Render(); 
+		const SDL_Point GetPos() { return m_pos; }
+
+};
+#endif
